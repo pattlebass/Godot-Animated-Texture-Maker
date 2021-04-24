d8756344391d52e379044e758488dd68f8e9f563
pattlebass <49322676+pattlebass@users.noreply.github.com>
2021-04-24 16:57:40 +0300

    Add files

---
+++ Godot AnimatedTexture Maker/main.gd
@@ -0,0 +1,55 @@
+extends Control
+
+
+var format := "%s"
+var fps: int = 1
+var oneshot:bool = false
+onready var texture := AnimatedTexture.new()
+
+func _on_LineEdit_text_changed(new_text):
+	if "%s" in new_text:
+		format = new_text
+	print(format)
+
+func generate(input_files: PoolStringArray):
+	#var texture = AnimatedTexture.new()
+	
+	texture.frames = input_files.size()
+	
+	for i in input_files.size():
+		var tex = ImageTexture.new()
+		var img = Image.new()
+		img.load(input_files[i])
+		tex.create_from_image(img)
+		texture.set_frame_texture(i, tex)
+	
+	$TextureRect.texture = texture
+
+func _on_SaveButton_pressed():
+	var file_name = "AnimatedTexture %s.%s.%s.tres" % [OS.get_datetime().hour, OS.get_datetime().minute, OS.get_datetime().second]
+	var dir_path = OS.get_executable_path().get_base_dir().plus_file("output")
+	
+	var dir = Directory.new()
+	dir.make_dir(dir_path)
+	ResourceSaver.save(dir_path.plus_file(file_name), texture)
+
+
+#func change_settings(fps: int, oneshot: bool):
+#	texture.fps = fps
+#	texture.oneshot = oneshot
+
+
+func _on_FileDialog_files_selected(paths):
+	print(paths)
+	generate(paths)
+
+
+func _on_ImportButton_pressed():
+	$FileDialog.popup_centered()
+
+func _on_FpsSpinBox_value_changed(value):
+	texture.fps = value
+
+func _on_OneshotCheckButton_toggled(button_pressed):
+	texture.oneshot = button_pressed
+
